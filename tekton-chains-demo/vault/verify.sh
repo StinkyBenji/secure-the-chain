cdbaa64cdbe709a6f1fef490613c25afe042c980
StinkyBenji <ether852@gmail.com>
2024-07-17 17:08:18 +0200

    feat: add verify command

---
+++ tekton-chains-demo/vault/verify.sh
@@ -0,0 +1,4 @@
+cosign verify-attestation --insecure-ignore-tlog=true --type slsaprovenance --key hashivault://cosign quay.io/stbenji/pipelines-vote-api-vault:0.0.2
+
+cosign tree quay.io/stbenji/pipelines-vote-api-vault:0.0.2
+
