d4fe494a96daed6770d24453a2855dce01730af4
StinkyBenji <ether852@gmail.com>
2024-07-18 10:45:51 +0200

    doc: add readme to tekton chains demo with vault

---
+++ hashicorp-vault/config/configure-vault.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export VAULT_ADDR=<YOUR VAULT URL>
+export VAULT_TOKEN=<YOUR VAULT TOKEN>
+
+echo "enable transit secret engine for cosign ----- "
+
+vault secrets enable transit
+vault write -f transit/keys/cosign type="rsa-2048"
+
+vault policy write cosign ./cosign.hcl
+
+export COSIGN_TOKEN=$(vault token create -policy=cosign -format=yaml -orphan -ttl=768h | grep -E "client_token" | sed 's/^.*://')
